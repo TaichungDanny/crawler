5693c7f312c2afbb4412ae03c14c2d6e4c1effd8
TaichungDanny <d033016@gmail.com>
2023-07-03 20:49:00 +0800

    add getting captcha picture

--- crawler.py
+++ crawler.py
@@ -4,10 +4,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.ui import Select
 from selenium.common.exceptions import NoSuchElementException
+from PIL import ImageGrab
 from PIL import Image
 import time
 import random
 import winsound
+import json
+import cv2 as cv
+import sys
 
 url = input('Entering url: ')
 ticnum = input('Entering ticket number: ')
@@ -21,6 +25,17 @@ def check_exists_by_xpath(xpath):
     except NoSuchElementException:
         return False
 
+
+def captcha_recognize():
+    img = "D:/Program Files//Sublime//Code//crawler//image.bmp"
+    img_1 = cv.imread("image.bmp")
+    if img_1 is None:
+        sys.exit("Could not read the image.")
+    else:
+        cv.imshow("Display window", img_1)
+        cv.waitKey(0)
+
+
 chromedriver = 'chromedriver.exe'
 options = webdriver.ChromeOptions()
 options.add_argument("--disable-blink-features=AutomationControlled")
@@ -29,7 +44,15 @@ options.add_experimental_option("useAutomationExtension", False)
 driver = webdriver.Chrome(options=options)
 driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
 chains = ActionChains(driver)
+with open('cookies.json') as f:
+    cookies = json.load(f)
 driver.get(url)
+driver.maximize_window()
+
+
+for cookie in cookies:
+    driver.add_cookie(cookie)
+driver.refresh()
 
 
 while True:
@@ -39,14 +62,16 @@ while True:
         except:
             continue
         if driver.current_url != url:
-            chk = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[3]/div/div/div/form/div[3]')
+            time.sleep(4)
+            chk = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[3]/div/div/div/form/div[3]/div')
             chk.click()
             select = Select(driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[3]/div/div/div/form/div[1]/table/tbody/tr/td[2]/select'))
             select.select_by_visible_text(ticnum)
-            scroll_width = driver.execute_script('return document.body.parentNode.scrollWidth')
-            scroll_height = driver.execute_script('return document.body.parentNode.scrollHeight')
-            driver.set_window_size(scroll_width, scroll_height)
-            driver.save_screenshot('image.png')
+            img = ImageGrab.grab(bbox=(730, 660, 840, 770))
+            img.save('image.bmp')
+            cap = captcha_recognize()
+            # /html/body/div[2]/div[1]/div[3]/div/div/div/form/div[2]/div[2]/input
+
             order = driver.find_element(By.XPATH, '/html/body/div[2]/div[1]/div[3]/div/div/div/form/div[4]/button[2]')
             # order.click()
             # for i in range(10):
@@ -60,6 +85,9 @@ while True:
             else:
                 driver.refresh()
                 print('Refreshing!')
+
+captcha_recognize()
+time.sleep(50)
 
 
 
